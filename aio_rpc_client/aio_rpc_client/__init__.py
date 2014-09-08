1be6f0d64df121e62407ff7b65ecc1b89dfa040e
Ghassen Telmoudi <ghassen.telmoudi@gmail.com>
2014-09-08 12:39:03 +0100

    Making things simple and working

--- aio_rpc_client/aio_rpc_client/__init__.py
+++ aio_rpc_client/aio_rpc_client/__init__.py
@@ -11,21 +11,20 @@ class RPCClient(object):
         """
 
         self.uri = uri
-
-    def __getattr__(self, name, *args):
-        print("." * 100)
-        print(name)
-        print("." * 100)
-        # callback = self.call(name, *args)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.call(name, *args))
-        # loop.close()
         
     @asyncio.coroutine
-    def call(self, function_name, *args):
+    def __async_call(self, function_name, *args):
         """
         """
         client = yield from rpc.connect_rpc(connect=self.uri)
         result = yield from client.call.__getattr__(function_name)(*args)
-        print(result)
         return result
+
+    def call(self, function_name, *args):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(
+            self.__async_call(
+                function_name,
+                *args
+            )
+        )
